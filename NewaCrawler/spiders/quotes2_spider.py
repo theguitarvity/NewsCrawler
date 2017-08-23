beb1eca81b2b5563d9a27d3483305cb8f70745a8
Victor Lopes <contato_victorlopes@hotmail.com>
2017-08-23 13:54:43 -0400

    Testando extracao

---
+++ NewaCrawler/spiders/quotes2_spider.py
@@ -0,0 +1,18 @@
+import scrapy
+
+class Quotes2Scrapy(scrapy.Spider):
+    name = 'quotes2'
+    start_urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+    ]
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tag a.tag::text').extract_first(),
+            }
+    
+        
+
